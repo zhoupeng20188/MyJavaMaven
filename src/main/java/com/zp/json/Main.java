498c1c2de142cfff03e2674d0157af959904570f
zhoupeng <287214764@qq.com>
2021-06-07 22:02:00 +0800

    正则匹配${}

--- src/main/java/com/zp/json/Main.java
+++ src/main/java/com/zp/json/Main.java
@@ -2,6 +2,9 @@ package com.zp.json;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         String json = "{\n" +
@@ -25,6 +28,17 @@ public class Main {
 //        System.out.println(JsonUtil.getValue(jsonObject,"b.c",true));
         System.out.println(MyJsonParser.getValue(jsonObject,"b.c"));
         System.out.println(MyJsonParser.getValue(jsonObject,"c"));
+
+        String s = "{id:${id}, name:${name}";
+        // 匹配方式
+        Pattern p = Pattern.compile("\\$\\{(.*?)}");
+        // 匹配】
+        Matcher matcher = p.matcher(s);
+        // 处理匹配到的值
+        while (matcher.find()) {
+            System.out.println(matcher.group().replace("${","")
+            .replace("}", ""));
+        }
 
     }
 }
